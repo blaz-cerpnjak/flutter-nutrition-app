2e40616af847337b5478180b7e8272f52f458537
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-06-28 08:56:02 +0200

    Add bottom navigation bar

--- nutrition_app/lib/main.dart
+++ nutrition_app/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:nutrition_app/theme/theme_constants.dart';
 import 'package:nutrition_app/theme/theme_manager.dart';
 
@@ -69,26 +69,85 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  int index = 0;
+
+  void onTap(int newIndex) {
+    setState(() {
+      index = newIndex;
+    });      
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          color: Theme.of(context).primaryColor,
+          onPressed: () {},
+          icon: Padding(
+            padding: const EdgeInsets.all(8),
+            child: SvgPicture.asset(
+              "assets/icons/menu_icon.svg",
+              color: Theme.of(context).primaryColor,
+            )
+          )
+        ),
+      ),
       backgroundColor: Theme.of(context).backgroundColor,
       body: Center(
         child: 
           TextButton(
             onPressed: () { toggleTheme(); },
-            child: Text("Switch theme"),
+            child: const Text("Switch theme"),
           )
       ),
-      bottomNavigationBar: CurvedNavigationBar(
-        backgroundColor: Theme.of(context).backgroundColor,
-        color: Theme.of(context).bottomAppBarColor,
-        items: const [
-          Icon(Icons.home_rounded),
-          Icon(Icons.add_rounded),
-          Icon(Icons.person_rounded),
-        ]
-      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        selectedItemColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Theme.of(context).backgroundColor,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Theme.of(context).bottomAppBarColor,
+        elevation: 0,
+        onTap: onTap,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/home_icon.svg",
+              color: index == 0 ? Theme.of(context).primaryColor : Colors.grey.withOpacity(0.5),
+              width: index == 0 ? 17 : 15,
+            ),
+            label: "Home"  
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/food_icon.svg",
+              color: index == 1 ? Theme.of(context).primaryColor : Colors.grey.withOpacity(0.5),
+              width: index == 1 ? 17 : 15,
+            ),
+            label: "Food"  
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/add_icon.svg",
+              color: index == 2 ? Theme.of(context).primaryColor : Colors.grey.withOpacity(0.5),
+              width: index == 2 ? 17 : 15,
+            ),
+            label: "Add"  
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              "assets/icons/user_icon.svg",
+              color: index == 3 ? Theme.of(context).primaryColor : Colors.grey.withOpacity(0.5),
+              width: index == 3 ? 17 : 15,
+            ),
+            label: "User"  
+          ),
+        ],
+      )
     );
   }
 }
---
+++ nutrition_app/lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu_rounded)
+        ),
+      ),
+    );
+  }
+}
