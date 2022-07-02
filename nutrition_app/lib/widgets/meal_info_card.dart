6952b97248a379966fd392ad085da3c01bb5236b
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-07-02 14:05:04 +0200

    Fix onTap

--- nutrition_app/lib/widgets/meal_info_card.dart
+++ nutrition_app/lib/widgets/meal_info_card.dart
@@ -27,38 +27,41 @@ class _MealInfoCardState extends State<MealInfoCard> {
     String fats = ((widget._meal.food.fats/ 100) * widget._meal.quantity).toStringAsFixed(2); 
 
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  widget._meal.food.title,
-                  style: Theme.of(context).textTheme.subtitle2,
-                ),
-                Text(
-                  cals.toString(),
-                  style: Theme.of(context).textTheme.bodyText2,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "${widget._meal.quantity} g",
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-                Text(
-                  "C: $carbs g, P: $protein g, F: $fats g",
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-              ],
-            ),
-          ],
+      child: InkWell(
+        onTap: widget._onEditPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget._meal.food.title,
+                    style: Theme.of(context).textTheme.subtitle2,
+                  ),
+                  Text(
+                    cals.toString(),
+                    style: Theme.of(context).textTheme.bodyText2,
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "${widget._meal.quantity} g",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                  Text(
+                    "C: $carbs g, P: $protein g, F: $fats g",
+                    style: Theme.of(context).textTheme.bodySmall,
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       )
     );
