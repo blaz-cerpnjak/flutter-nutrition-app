c417a81342646390e8adf316599024bb75631a68
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2022-07-05 11:12:43 +0200

    Implement saving daily calories to prefs

--- nutrition_app/lib/screens/user_profile_screen.dart
+++ nutrition_app/lib/screens/user_profile_screen.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({Key? key}) : super(key: key);
-
+  
   @override
   State<UserProfileScreen> createState() => _UserProfileScreenState();
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
+  final TextEditingController _dailyCalsController = TextEditingController();
+  late double calories;
+
+  @override
+  void initState() {
+    getCalories();
+    super.initState();
+  }
+
+  Future<void> getCalories() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    calories = prefs.getDouble("calories") ?? 2100;
+    _dailyCalsController.text = calories.toString();
+  }
+
+  Future<void> saveCalories() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setDouble("calories", double.parse(_dailyCalsController.text));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: const Center(
-        child: Text("User Profile Screen"),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text('Daily Calories:'),
+              const SizedBox(width: 150),
+              Flexible(child: TextField(
+                controller: _dailyCalsController,
+                style: Theme.of(context).textTheme.bodyText1,
+                keyboardType: TextInputType.number,
+                onSubmitted: (s) {
+                  saveCalories();
+                },
+              )),
+            ],
+          )
+        ),
       )
     );
   }
